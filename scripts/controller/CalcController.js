92edfce4c17e3e1fc2bd87cbb681efb37b62aebe
HCesar14 <henriquecesar14@gmail.com>
2018-07-04 16:14:30 -0300

    terceiro commit

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,13 +2,14 @@ class CalcController {
 
     constructor(){
       
-        this._displayCalcEl = document.querySelector("#display");
+        this._displayCalcEl = document.querySelector("#display"); // _ => padrao quando é private
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora"); 
         this._currentDate;
         this._locale = 'pt-BR';
 
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -20,6 +21,33 @@ class CalcController {
           this.setDisplayDateTime();
 
         }, 1000); //att a cada segundo
+    }
+
+    addEventListenerAll(element, events, fn){ //botao, evento, funcao ao ocorrer o evento
+
+        events.split(' ').forEach(event => { //separa pelo " " espaço vazio
+
+            element.addEventListener(event, fn, false); //cria o evento passando o evento atual(click, drag), executa a função do parametro e 
+            //false como tem o btn e o texto do btn acontecendo em 1 deles ja aborta o outro
+        });
+    }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g"); // All traz todos, pega id buttons, > filhos g
+
+        buttons.forEach((btn, index) => {
+
+            this.addEventListenerAll(btn, 'click drag', e => { //metodo criado aqui
+
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+
+                btn.style.cursor = 'pointer';
+            });
+        });
     }
 
     setDisplayDateTime(){
